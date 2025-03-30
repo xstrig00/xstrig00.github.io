c30483fd3b1d949b43e6c411d40aff37d7e5033b
astrigac <aurel.strigac@proton.me>
2025-03-30 23:16:11 +0200

    reverting the USA changes

--- js/Mouseflow/ef6ef5b9-cd69-43f2-90d3-fc6cfbd9faa8.js
+++ js/Mouseflow/ef6ef5b9-cd69-43f2-90d3-fc6cfbd9faa8.js
@@ -5,7 +5,7 @@ if (typeof mouseflow === 'undefined' && typeof mouseflowPlayback === 'undefined'
         var _462 = false;
         var _470 = [];
         var _471 = [];
-        var _47 = 'https://us.mouseflow.com';//CHANGE 'https://eu.mouseflow.com';
+        var _47 = 'https://eu.mouseflow.com';
         const _407 = window.location.search.slice(1).split('&').map(a => a.split('=', 2)).filter(a => a[0]).reduce( (acc, cur) => (acc[cur[0]] = cur[1] ?? null,
         acc), {});
         function _7(_154, _14) {
@@ -36,7 +36,7 @@ if (typeof mouseflow === 'undefined' && typeof mouseflowPlayback === 'undefined'
         var _283 = new _1047(window,_11,_3,_7);
         function _560(_2, _105, _28, _3, _25, _11, _23, _88, _272, _55, _184, _218, _484, _7, _410, _283, _120) {
             var _1142 = false;
-            var _175 = 'https://us01.rec.mouseflow.com/';//CHANGE 'https://eu01.rec.mouseflow.com/';
+            var _175 = 'https://eu01.rec.mouseflow.com/';
             var _241 = 100.0;
             var _178 = [];
             var _862 = [];
